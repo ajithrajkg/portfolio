2ff7bb80fccc3b0817c79c1903ae3532a0235433
Ajith Raj <your-email@gmail.com>
2026-09-06 15:09:37 +0530

    fix contact route

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['192.168.1.12', 'localhost', '127.0.0.1'],
+  trailingSlash: true,
 };
 
 export default nextConfig;
